310e7365375ad6262fda2ecf35effe0c6d42cd2a
Pedro <pedroandrisano@gmail.com>
2019-12-09 12:16:39 -0300

    adivina_el_numero.py

--- adivina_el_numero.py
+++ adivina_el_numero.py
@@ -1,14 +1,4 @@
-<<<<<<< HEAD
-number_to_guess = 2
 
-user_number = int(input("adivina un numero del 1 al 10: "))
-
-if number_to_guess == user_number:
-    print("You Won")
-else:
-    print("Try Again")
-
-=======
 number_to_guess = 2
 
 user_number = int(input("adivina un numero del 1 al 10: "))
@@ -17,4 +7,4 @@ if number_to_guess == user_number:
     print("You Won")
 else:
     print("You Lose")
->>>>>>> origin/master
+
